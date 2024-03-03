f71b98320077936d882fe9d928f365255ed5e709
frehywet jekiso <156281010+frehywet@users.noreply.github.com>
2024-03-03 12:43:05 -0500

    Add files via upload

---
+++ Module # 8 Input:Output, string manipulation and plyr package .R
@@ -0,0 +1,18 @@
+# load the plyr package
+library(plyr)
+#import the txt file and assign to txtStudentData
+txtStudentData <- read.table("~/Downloads/Assignment 6 Dataset.txt", header = TRUE,sep = ",")
+txtStudentData
+
+#calculate average by sex
+StudentAverage = ddply(txtStudentData,"Sex", transform, Grade.Average=mean(Grade))
+StudentAverage
+#write file as csv, saved in working directory
+write.table(StudentAverage, "SortedAverage.csv")
+
+#filter Data to include only students that contain "i" in their name
+i_students <- subset(txtStudentData, grepl("i", txtStudentData$Name, ignore.case=T))
+i_students
+
+#write file as csv, saved in working directory
+write.csv(i_students, "i_students.csv")
